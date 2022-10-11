b021c9a2b6e7a2666c0492aeb9acb57514afc481
Kennedy <60758839+kennedy354@users.noreply.github.com>
2022-10-11 15:09:46 -0300

    Add files via upload

---
+++ Pweb2 - lista 1/Q7/script.js
@@ -0,0 +1,12 @@
+let b= parseInt(prompt("Digite a Base"));
+let a= parseInt(prompt("Digite a Altura"));
+
+area=0
+perimetro=0
+
+area=b*a
+perimetro=2*(b+a)
+
+document.write("A area é ",area)
+document.write("<br></br>")
+document.write("O Perimetro é ",perimetro)
